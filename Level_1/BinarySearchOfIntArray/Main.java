f2a295c4d32286526a48aaa160c2f16e8b3b0685
stover74 <stover74@gmail.com>
2018-08-25 12:43:50 -0400

    Added Binary Search Problem to Level_1
    Completed Binary Search Problem and passed all Unit Tests.

---
+++ Level_1/BinarySearchOfIntArray/Main.java
@@ -0,0 +1,57 @@
+package BinarySearchOfIntArray;
+import java.util.*;
+
+/**
+ * Binary Search on Array of Integers 
+ * 
+ * Write a method that searches an Array of integers for a given integer using the
+ * Binary Search Algorithm. If the input integer is found in the array, return true. Otherwise, return false.
+ * You can assume that the given array of integers is already sorted
+ * in ascending order.
+ *
+ * Examples:
+ *
+ * binarySearch({2,5,7,8,9},9) -> true
+ *
+ * binarySearch({2,8,9,12},6) -> false
+ * 
+ * binarySearch({2},4) -> false
+ *
+ * binarySearch({},9) -> false
+ *
+ * {} -> [Empty] Array 
+ *
+ *
+ * @author Joe
+ * @version 08/25/2018
+ */
+public class Main 
+{
+	public static Boolean binarySearch(int[] arr, int n)
+	{
+		int first = 0;
+		int last = arr.length - 1;
+		while(first <= last)
+		{
+			int targetIndex = (last + first) / 2;
+			if(arr[targetIndex] == n)
+			{
+				return true;
+			}
+			else if(arr[targetIndex] < n)
+			{
+				first = targetIndex + 1;
+			}
+			else
+			{
+				last = targetIndex - 1;
+			}
+		}
+		return false;
+	}
+	public static Boolean binarySearchCheat(int[] arr, int n)
+	{
+		// of course, you could just use the Java API
+		return Arrays.binarySearch(arr, n) >= 0;
+	}
+}
---
+++ Level_1/BinarySearchOfIntArray/MainTest.java
@@ -0,0 +1,46 @@
+package BinarySearchOfIntArray;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class MainTest 
+{
+	@Test
+	public void testOne() 
+	{
+		int[] arr = {2,5,7,8,9};
+		assertTrue(Main.binarySearch(arr, 9));
+	}
+	
+	@Test
+	public void testTwo() 
+	{
+		int[] arr = {2,8,9,12};
+		assertFalse(Main.binarySearch(arr, 6));
+	}
+	
+	@Test
+	public void testThree() 
+	{
+		int[] arr = {2};
+		assertFalse(Main.binarySearch(arr, 4));
+	}
+	
+	@Test
+	public void testFour() 
+	{
+		int[] arr = {};
+		assertFalse(Main.binarySearch(arr, 9));
+	}
+	
+	@Test
+	public void testFive() 
+	{
+		int[] arr = new int [0];
+		assertFalse(Main.binarySearch(arr, 9));
+	}
+
+}
